eb80a1901b1f346ab08ce51809be4eb18cd7ee81
Sudeepa <sudeepa6502@gmail.com>
2026-04-08 20:43:18 +0530

    Route management and conductor module completed

--- sltb/core/admin.py
+++ sltb/core/admin.py
@@ -1,20 +1,36 @@
 from django.contrib import admin
-from .models import Bus, Driver
+from .models import Bus, Driver, Conductor, Route, Stop, Schedule
 
 
 @admin.register(Bus)
 class BusAdmin(admin.ModelAdmin):
     list_display = ('bus_code', 'bus_number', 'status')
-
+    #search_fields = ('bus_number',)
+    #list_filter = ('status',)
 
 @admin.register(Driver)
 class DriverAdmin(admin.ModelAdmin):
-    list_display = (
-        'driver_name',
-        'driving_license_number',
-        'phone_number',
-        'email',
-        'driver_status'
-    )
+    list_display = ('driver_name','driving_license_number','phone_number','email','driver_status')
+    #search_fields =  ('name', 'license_number')
+    #list_filter = ('driver_status',)
+
+@admin.register(Conductor)
+class ConductorAdmin(admin.ModelAdmin):
+    list_display = ('conductor_name','c_phone_number','c_email','conductor_status')
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('route_number', 'start_location', 'end_location')
+
+
+@admin.register(Stop)
+class StopAdmin(admin.ModelAdmin):
+    list_display = ('stop_name', 'route', 'order')
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('route', 'bus', 'driver', 'date', 'depature_time')
+
 
 # Register your models here.
--- sltb/core/models.py
+++ sltb/core/models.py
@@ -1,33 +1,9 @@
 from django.db import models
 
-class Bus(models.Model):
-    BUS_TYPE_CHOICES = [
-        ('AC' , 'AC'),
-        ('NON_AC' , 'Non-AC'),
-    ]
-
-    STATUS_CHOICES = [
-        ('AVAILABLE' , 'Available'),
-        ('MAINTENANCE' , 'Maintenance'),
-        ('ON_ROUTE' , 'On Route'),
-    ]
-
-    bus_number = models.CharField(max_length=20, unique=True)
-    bus_code = models.CharField(max_length=10, unique=True)
-    model = models.CharField(max_length=50, blank=True, null=True)
-    capacity = models.PositiveBigIntegerField()
-    bus_type = models.CharField(max_length=10, choices=BUS_TYPE_CHOICES)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    depot = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='bus_images/', blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.bus_code} ({self.bus_number})"
-    
+#Driver model:
 class Driver(models.Model):
     DRIVER_STATUS_CHOICES = [
-        ('AVAIALABLE' , 'Available'),
+        ('AVAILABLE' , 'Available'),
         ('ON_ROUTE' , 'On Route'),
         ('OFF_DUTY' , 'Off Duty'),
     ]    
@@ -60,6 +36,106 @@ class Driver(models.Model):
 
     def __str__(self):
         return f"{self.driver_name} ({self.driving_license_number})"
+    
+#Conductor model:
+class Conductor(models.Model):
+    CONDUCTOR_STATUS_CHOICES = [
+        ('AVAILABLE' , 'Available'),
+        ('ON_DUTY' , 'On Duty'),
+        ('OFF_DUTY' , 'Off Duty'),
+    ]    
+
+    GENDER_CHOICES = [
+        ('MALE' , 'Male'),
+        ('FEMALE' , 'Female'),
+    ]
+
+    conductor_name = models.CharField(max_length=100)
+    c_dob = models.DateField()
+    c_nic_number = models.CharField(max_length=20, unique=True)
+    c_gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    c_phone_number = models.CharField(max_length=10)
+    c_email = models.EmailField(blank=True, null=True)
+    conductor_address = models.TextField(blank=True, null=True)
+    conductor_status = models.CharField(
+        max_length=20,
+        choices=CONDUCTOR_STATUS_CHOICES,
+        default='AVAILABLE'
+    )
+    conductor_registration_date = models.DateField()
+    conductor_id_image = models.ImageField(
+        upload_to='driver_ids/',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.conductor_name
+    
+#Bus Model: 
+class Bus(models.Model):
+    BUS_TYPE_CHOICES = [
+        ('AC' , 'AC'),
+        ('NON_AC' , 'Non-AC'),
+    ]
+
+    STATUS_CHOICES = [
+        ('AVAILABLE' , 'Available'),
+        ('MAINTENANCE' , 'Maintenance'),
+        ('ON_ROUTE' , 'On Route'),
+    ]
+
+    bus_number = models.CharField(max_length=20, unique=True)
+    bus_code = models.CharField(max_length=10, unique=True)
+    model = models.CharField(max_length=50, blank=True, null=True)
+    capacity = models.PositiveBigIntegerField()
+    bus_type = models.CharField(max_length=10, choices=BUS_TYPE_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    depot = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='bus_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #Linking driver to bus:
+    #driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.bus_code} ({self.bus_number})"
+    
+#Route class:
+class Route(models.Model):
+    route_number = models.CharField(max_length=10)
+    start_location = models.CharField(max_length=100)
+    end_location = models.CharField(max_length=100)
+    distance = models.FloatField(null=True, blank=True)
+    estimated_duration = models.DurationField()
+
+    def __str__(self):
+        return self.route_number
+    
+#Stop class: 
+class Stop(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    stop_name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    order = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return f"{self.stop_name} ({self.route.route_number})"
+
+#Schedule class:
+class Schedule(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    bus = models.ForeignKey('Bus', on_delete=models.CASCADE)
+    driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
+    depature_time = models.TimeField()
+    arrival_time = models.TimeField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.route.route_number} - {self.date}"
+
 
 
 
