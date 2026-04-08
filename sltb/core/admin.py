cd1a41f8a5299db24053c9f43defc99372df1719
Sudeepa <sudeepa6502@gmail.com>
2026-04-08 13:27:13 +0530

    Driver module foundation done

--- sltb/core/admin.py
+++ sltb/core/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-
-from .models import Bus
-admin.site.register(Bus)
+from .models import Bus, Driver
 
 
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('bus_code', 'bus_number', 'status')
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = (
+        'driver_name',
+        'driving_license_number',
+        'phone_number',
+        'email',
+        'driver_status'
+    )
 
 # Register your models here.
--- sltb/core/models.py
+++ sltb/core/models.py
@@ -24,4 +24,42 @@ class Bus(models.Model):
 
     def __str__(self):
         return f"{self.bus_code} ({self.bus_number})"
+    
+class Driver(models.Model):
+    DRIVER_STATUS_CHOICES = [
+        ('AVAIALABLE' , 'Available'),
+        ('ON_ROUTE' , 'On Route'),
+        ('OFF_DUTY' , 'Off Duty'),
+    ]    
+
+    GENDER_CHOICES = [
+        ('MALE' , 'Male'),
+        ('FEMALE' , 'Female'),
+    ]
+
+    driver_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    nic_number = models.CharField(max_length=20, unique=True)
+    driving_license_number = models.CharField(max_length=50, unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField(blank=True, null=True)
+    driver_address = models.TextField(blank=True, null=True)
+    driver_status = models.CharField(
+        max_length=20,
+        choices=DRIVER_STATUS_CHOICES,
+        default='AVAILABLE'
+    )
+    driver_registration_date = models.DateField()
+    driver_id_image = models.ImageField(
+        upload_to='driver_ids/',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.driver_name} ({self.driving_license_number})"
+
+
 
